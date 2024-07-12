cc8194f87abb84f265e55281d4b5a440b4024116
TTimelord <759074800@qq.com>
2024-07-12 20:18:28 +0800

    add plot error

--- legged_controllers/scripts/plot_log.py
+++ legged_controllers/scripts/plot_log.py
@@ -24,6 +24,25 @@ def plot_data(data, indices, column_names):
     plt.tight_layout()
     plt.show()
 
+def plot_data_error(data, indices, indices_2, column_names, column_names_2):
+    times = data[:, 0]  # get time stamp
+    times -= times[0]  # shift to zero
+
+    assert len(indices) == len(column_names)
+
+    plt.figure(figsize=(10, 6))
+    for i, index in enumerate(indices):
+        plt.subplot(len(indices), 1, i + 1)
+        plt.plot(times, data[:, index], label=column_names[i])
+        plt.plot(times, data[:, indices_2[i]], label=column_names_2[i])
+        # plt.xlabel('Time (s)')
+        plt.ylabel(column_names[i])
+        # plt.title(f'Time Series Plot for {column_names[i]}')
+        plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
 def main():
     log_dir = os.path.join(os.path.expanduser('~'), 'mpc_log')
     file_name = 'output_2024-07-11_14-22-03.csv'
@@ -41,8 +60,8 @@ def main():
     # indices = [25,26,27,28,29,30]
     # column_names = ['v_l1', 'l2', 'l3', 'l4', 'l5', 'l6']
 
-    indices = [37,38,39,40,41,42]
-    column_names = ['error_l1', 'l2', 'l3', 'l4', 'l5', 'l6']
+    # indices = [37,38,39,40,41,42]
+    # column_names = ['error_l1', 'l2', 'l3', 'l4', 'l5', 'l6']
 
     data = read_csv_numpy(file_path)
 
@@ -50,9 +69,10 @@ def main():
     selected_end_time = 15
     frequency = 400
     selected_data = data[selected_start_time*frequency:selected_end_time*frequency, :]
-    selected_data[:, 37:43] = selected_data[:, 13:19] - selected_data[:, 37:43]
+    # selected_data[:, 37:43] = selected_data[:, 13:19] - selected_data[:, 37:43]
 
-    plot_data(selected_data, indices, column_names)
+    # plot_data(selected_data, indices, column_names)
+    plot_data_error(selected_data, [13,14,15,16,17,18], [37,38,39,40,41,42], ['l1', 'l2', 'l3', 'l4', 'l5', 'l6'], ['cmd_l1', 'l2', 'l3', 'l4', 'l5', 'l6'])
 
 if __name__ == '__main__':
     main()
